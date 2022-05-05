2e820fb1fd32223f7e354c925de773e8837ada75
Gokhan Turkhan <gokhan_turkhan86@hotmail.com>
2022-05-06 00:08:37 +0300

    KurumsalProje ver5 - Validations ve Autofac

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluendValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -15,17 +19,18 @@ namespace Business.Concrete
     {
         IProductDal _productDal;  
         public ProductManager(IProductDal productDal)
-        {
+        { 
             _productDal = productDal;
         }
 
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if(product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+
+            
+            ValidationTool.Validate(new ProductValidator(), product);
+
             _productDal.Add(product);
             
             return new SuccessResult(Messages.ProductAdded);
